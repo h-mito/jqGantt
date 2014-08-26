efd61f089951fb515819e00427997dfe862a6a13
mito <hiroshi.mito@gmail.com>
2014-08-26 23:05:02 +0900

    Rails initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JqGantt::Application.config.secret_key_base = '93fb6bc16da8504482053a20fba1ef12a17c4a86ba9ca1d672f755f39b4e7c8062b77ab1175c70592756c9e96e09a43635e6090a7c94bd998581e2dc7ec577f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JqGantt::Application.config.session_store :cookie_store, key: '_jqGantt_session'
